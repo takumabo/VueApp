f80abdb0d84c80dba3bd83a282dee4bf90e2be20
takumabo <kingcongyatuhashi.1043@gmail.com>
2020-12-05 12:53:46 +0900

    Vueの基礎完了

--- index.js
+++ index.js
@@ -5,30 +5,137 @@ Vue.createApp({
             todoDescription: '',
             todoCategories: [],
             hideDoneTodo: false,
-            order:'',
+            order:'desc',
             selectedCategory:'',
-            searchWord: ''
+            todos:[],
+            categories:[],
+            searchWord: '',
+            categoryName: ''
          }
     },
     computed:{
         canCreateTodo:function(){
             return this.todoTitle !== ''
         },
+        canCreateCategory: function(){
+            return this.categoryName !== '' && !this.existsCategory
+        },
+
+        existsCategory: function(){
+            const categoryName = this.categoryName
+            return this.categories.indexOf(categoryName) !== -1
+        },
+
+        hasTodos: function(){
+            return this.todos.length > 0
+        },
+        resultTodos: function(){
+            const selectedCategory = this.selectedCategory
+            console.log(selectedCategory)
+            const hideDoneTodo = this.hideDoneTodo
+            const order = this.order
+            const searchWord = this.searchWord
+            return this.todos
+            .filter(function(todo){
+                return(
+                    selectedCategory === '' || todo.categories.indexOf(selectedCategory) !== -1
+                )
+            })
+            .filter(function(todo){
+                if(hideDoneTodo){
+                    return !todo.done
+                }
+                return true
+            })
+            .filter(function(todo){
+                return (
+                    todo.title.indexOf(searchWord) !== -1 || todo.description.indexOf(searchWord) !== -1
+                )
+            })
+            .sort(function(a,b){
+                if(order === 'asc'){
+                    return a.dateTime - b.dateTime
+                }
+                return b.dateTime - a.dateTime
+            })
+        },
+    },
+
+    watch: {
+        todos: {
+            handler: function(next){
+                window.localStorage.setItem('todos',JSON.stringify(next))
+            },
+            deep: true
+        },
+        categories: {
+            handler: function(next){
+                window.localStorage.setItem('categories',JSON.stringify(next))
+            },
+            deep: true
+        }
+    },
+    methods:{
+        createTodo: function(){
+            if(!this.canCreateTodo){
+                return
+            }
+
+            this.todos.push({
+                id: 'todo-' + Date.now(),
+                title: this.todoTitle,
+                description: this.todoDescription,
+                categories: this.todoCategories,
+                dateTime: Date.now(),
+                done: false,
+            })
+
+            this.todoTitle = ''
+            this.todoDescription = ''
+            this.todoCategories = []
+        },
+        createCategory: function() {
+            if(!this.canCreateCategory){
+                return
+            }
+
+            this.categories.push(this.categoryName)
+
+            this.categoryName = ''
+        }
+    },
+    created: function(){
+        const todos = window.localStorage.getItem('todos')
+        const categories = window.localStorage.getItem('categories')
+
+        if(todos){
+            this.todos = JSON.parse(todos)
+        }
+
+        if(categories){
+            this.categories = JSON.parse(categories)
+        }
     },
 }).mount('#app')
 
-
+// Vue.createApp({}).component('my-title',{
+//     data:function(){
+//         return {
+//             title: ''
+//         }
+//     },
+//     template: `<input type="text" v-model="title" />`,
+// }).mount('#sample')
 
 Vue.createApp({
-    data:function(){
-        return{
-            counter: 0
-        }
-    },
-    methods: {
-        onClickCountUp: function(event){
-            console.log(event)
-            return this.counter += 1
+    components: {
+        'my-title':{
+            data: function(){
+                return {
+                    titile: ''
+                }
+            },
+            template: `<input type="text" v-model="title"/>`,
         }
     }
 }).mount('#sample')
